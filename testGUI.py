53895d7e35e331044019b17f0aca5fa49f5208b5
Thai To <imswaggy842@gmail.com>
2021-12-11 22:35:58 +0700

    update TestGUI.py
    + Cần ráp code vào

--- testGUI.py
+++ testGUI.py
@@ -1,7 +1,9 @@
 from cgitb import text
 from dataclasses import Field
+from typing import Literal
 from contact import *
 import tkinter as tk
+import random
 from PIL import Image, ImageTk
 
 test1 = Contact("Hao", "492", "12345", "1mail", b'')
@@ -9,40 +11,63 @@ test2 = Contact("Thai", "987", "12345", "2mail", b'')
 testList1 = [test1, test2, test1, test2, test1, test2]
 testList2 = [test1, test2]
 
+Grey = "#E8E8E8"
+LightGrey = "#f0f0f0"
+Navy = "#14279B"
+Bluely = "#3D56B2"
+SkyBlue = "#5C7AEA"
+
 mainWindow = tk.Tk()
 
+#main window
+mainWindow.title("Phone Book")
+mainWindow.geometry("1000x600")
+mainWindow.resizable(height=False, width=False)
+
+#Background
+mainCanvas = tk.Canvas(mainWindow,  width=1000,
+						height=600, bg=Bluely)
+mainCanvas.pack()
+
+#Foreground
+inputFrame = tk.Frame(mainCanvas, bg= LightGrey)
+inputFrame.place(relwidth=0.96, relheight=0.94, relx=0.02, rely=0.03)
 
 class InfoBar():
-    def __init__(self, parrent, width, name: str, phone: str, mail: str, id: str) -> None:
-        mainBar = tk.Frame(parrent, width=width, height=100, bg="red")
-        mainBar.pack(padx=5, pady=5)
-        self.width = width
-        self.height = 100
-        self.id = id
+	def __init__(self, parrent, width, name: str, phone: str, mail: str, id: str) -> None:
+		mainBar = tk.Frame(parrent, width=width, height=100, bg= Grey)
+		mainBar.pack(padx=5, pady=5)
+		self.width = width
+		self.height = 100
+		self.id = id
 
         #Labels
-        self.name = tk.Label(mainBar, text="Name: " + name,
-                             font=('Courier', 12), anchor=tk.W, bg="red")
-        self.phone = tk.Label(mainBar, text="Number: " +
-                              phone, font=('Courier', 12), anchor=tk.W, bg="red")
-        self.mail = tk.Label(mainBar, text="Email: " + mail,
-                             font=('Courier', 12), anchor=tk.W, bg="red")
-        self.id = tk.Label(mainBar, text="ID: " + id,
-                           font=('Courier', 12), anchor=tk.W, bg="red")
+		self.name = tk.Label(mainBar, text="Name: " + name,
+                             font=('Arial Bold', 12), anchor=tk.W, bg=Grey, fg="black")
+		self.phone = tk.Label(mainBar, text="Number: " +
+                              phone, font=('Arial', 12), anchor=tk.W, bg=Grey, fg="black")
+		self.mail = tk.Label(mainBar, text="Email: " + mail,
+                             font=('Arial', 12), anchor=tk.W, bg=Grey, fg="black")
+		self.id = tk.Label(mainBar, text="ID: " + id,
+                           font=('Arial Italic', 12), anchor=tk.W, bg=Grey, fg="black")
+
+        # Button "View Avatar"
+		avaBttn = tk.Button(mainBar, text="View Avatar",anchor= tk.W, command=MainGUI.ViewAva, bg=Bluely, fg="white")
 
         #Pic holder
-        self.picPlace = tk.Canvas(mainBar, bg= '#7e7e7e', width= 80, height= 80)
-        img = ImageTk.PhotoImage(Image.open("photo\\00001.jpg").resize((80, 80)))   #Temporally using 00001.jpg for testing
-        photo = tk.Label(self.picPlace, image= img)
-        photo.image = img
-        photo.pack()
+		self.picPlace = tk.Canvas(mainBar, bg= '#7e7e7e', width= 80, height= 80)
+		img = ImageTk.PhotoImage(Image.open("photo\\00001.jpg").resize((80, 80)))   #Temporally using 00001.jpg for testing
+		photo = tk.Label(self.picPlace, image= img)
+		photo.image = img
+		photo.pack()
 
-        #Placement
-        self.name.place(x=self.width / 3 + 10, y= self.height / 4, anchor= tk.W)
-        self.id.place(x=self.width / 3 * 2 + 10, y=self.height / 4, anchor= tk.W)
-        self.phone.place(x=self.width / 3 + 10, y=self.height / 4 * 2, anchor= tk.W)
-        self.mail.place(x=self.width / 3 + 10, y=self.height / 4 * 3, anchor= tk.W)
-        self.picPlace.place(x= self.width / 12, y= self.height / 2, anchor=tk.W)
+		#Placement
+		self.name.place(x=self.width / 3 + 10, y= self.height / 4, anchor= tk.W)
+		self.id.place(x=self.width - 80, y=self.height / 4, anchor= tk.W)
+		self.phone.place(x=self.width / 3 + 10, y=self.height / 4 * 2, anchor= tk.W)
+		self.mail.place(x=self.width / 3 + 10, y=self.height / 4 * 3, anchor= tk.W)
+		avaBttn.place(x=self.width - 80, y=self.height - 20, anchor=tk.W)
+		self.picPlace.place(x= self.width / 12, y= self.height / 2, anchor=tk.W)            
 
 
 class InfoBox():
@@ -52,7 +77,7 @@ class InfoBox():
 
         #Config
         self.container = tk.Frame(parrent)
-        self.mainBox = tk.Canvas(self.container, width=width, height=480, bg="yellow")
+        self.mainBox = tk.Canvas(self.container, width=width, height=480, bg= LightGrey)
         self.mainBox.pack_propagate(0)
 
         self.scrol = tk.Scrollbar(self.container, command= self.mainBox.yview)
@@ -91,56 +116,70 @@ class InfoBox():
 
 
 class MainGUI():
-    def __init__(self) -> None:
-        self.lst = testList1
+	def __init__(self) -> None:
+        
+		self.lst = testList1
+		
+		#Phone book text
+		phoneBookText = tk.Label(inputFrame, text='fonebook', font=("Arial Black", 40), fg= Bluely, bg = LightGrey)
+		phoneBookText.place(x=30, y=10)
 
-        #main window
-        mainWindow.title("Phone Book")
-        mainWindow.geometry("1000x600")
-        mainWindow.resizable(height=False, width=False)
+		#Pic place holder
 
-        #Background
-        mainCanvas = tk.Canvas(mainWindow,  width=1000,
-                               height=600, bg="#007dfe")
-        mainCanvas.pack()
+		#List Box
+		self.listBox = InfoBox(inputFrame, 450, 470, 75)
+		self.listBox.newList(testList1)
+		self.listBox.refeshBox()
 
-        #Foreground
-        inputFrame = tk.Frame(mainCanvas, bg="white")
-        inputFrame.place(relwidth=0.95, relheight=0.95, relx=0.025, rely=0.025)
+		#Test Button
+		refreshbttn = tk.Button(inputFrame, text= "Refresh", command= self.listBox.refeshBox)
+		refreshbttn.place (x= 362, y= 123)
 
-        #Phone book text
-        phoneBookText = tk.Label(
-            inputFrame, text='PHONE BOOK', font=("Courier", 40), bg="white")
-        phoneBookText.pack()
+		#Search bar
+		#Label
 
-        test = tk.Label(inputFrame, text="Testing",
-                        font=("Courier", 13), bg="white")
-        test.place(x=10, y=100)
+		srcLabel = tk.Label(
+			inputFrame, text="Name", font=("Arial", 11), bg= LightGrey, fg="black"
+		)
+		srcLabel.place(x= 34, y= 100)
 
-        test = tk.Label(inputFrame, text="Testing",
-                        font=("Courier", 13), bg="white")
-        test.place(x=10, y=100)
 
-        #Pic place holder
-        frame = tk.Frame(inputFrame, width=200, height=200, bg="#7e7e7e")
-        frame.place(x=100, y=350)
+		#Bar
+		srcBox = tk.Entry(
+			inputFrame,  width=44
+		)
+		#srcBox.place(x=85, y=106)
+		srcBox.place(x=36, y=126)
 
-        #List Box
-        self.listBox = InfoBox(inputFrame, 450, 470, 75)
-        self.listBox.newList(testList1)
-        self.listBox.refeshBox()
+		#Button
+		srcBttn = tk.Button(
+			inputFrame, text="Search", bg=Bluely, fg="white", command= MainGUI.search
+		)
+		srcBttn.place (x=310, y=123)
 
-        #Test Button
-        self.button = tk.Button(inputFrame, text= "Switch List", command= self.switchList)
-        self.button.pack()
+		mainWindow.mainloop()
 
-        mainWindow.mainloop()
+	def switchList(self):
+		self.lst = testList2
+		self.listBox.newList(testList2)
+		self.listBox.refeshBox()
+		
+	def search():
+		print("hello")
 
-    def switchList(self):
-        self.lst = testList2
-        self.listBox.newList(testList2)
-        self.listBox.refeshBox()
+
+	def ViewAva():
+		frame = tk.Frame(inputFrame, width=300, height=300, bg= "#" + str(random.randint(0, 9)) + "5ea4" + str(random.randint(0, 9)))
+		frame.place(x=75, y=200)
+
 
 
 if __name__ == "__main__":
     start = MainGUI()
+
+"""
+[X] Search bar
+[ ] List
+[ ] Options
+[X] Refresh
+"""
