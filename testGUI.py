a5433b552291b9b74c3fe07eca110798ce3defc1
Thai To <imswaggy842@gmail.com>
2021-12-12 14:22:20 +0700

    Update testGUI.py
    Đã xong

--- testGUI.py
+++ testGUI.py
@@ -1,5 +1,7 @@
+#from _typeshed import Self         #<<< Báo lỗi nên tạm thời vô hiệu hóa
 from cgitb import text
 from time import sleep
+from tkinter.font import Font
 from contact import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
@@ -25,14 +27,19 @@ COMMAND = {							#<<<<<< COMMAND 'FIND' sẽ đính kèm theo thông tin cần 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def connectSocket():
-	notConnected = True
-	while notConnected:
-		try:
-			serverSocket.connect(ADDR)
-			notConnected = False
-		except socket.error as msg:
-			print(msg)
-			sleep(5)
+    notCnnt = True
+    while notCnnt:
+        try:
+            serverSocket.connect(ADDR)
+            notCnnt = False
+            _msg = tk.Label(inputFrame, text="Connected!", font=("Arial", 10), bg=LightGrey)
+            _msg.place(x=883, y=40)
+        except socket.error as msg:
+            _warn = tk.Label(inputFrame, text="Already connected!", font=("Arial", 10), bg=LightGrey)
+            _warn.place(x=837, y=40)
+            print(msg)
+            break
+
 
 def closeSocket():
 	try:
@@ -90,6 +97,7 @@ def sendAndRecv(msg: str) -> list:							#<<<<<<<<<<<<< Sử dụng hàm này k�
 	result = pickle.loads(recvmsg)
 
 	return result
+
 class InfoBar():
     def __init__(self, parrent, width, name: str, phone: str, mail: str, id: str) -> None:
         mainBar = tk.Frame(parrent, width=width, height=100, bg=Grey)
@@ -121,67 +129,67 @@ class InfoBar():
         photo.pack()
 
         # Placement
-        self.name.place(x=self.width / 3 + 10, y=self.height / 4, anchor=tk.W)
+        self.name.place(x=self.width / 3 - 20, y=self.height / 4, anchor=tk.W)
         self.id.place(x=self.width - 80, y=self.height / 4, anchor=tk.W)
-        self.phone.place(x=self.width / 3 + 10,
+        self.phone.place(x=self.width / 3 - 20,
                          y=self.height / 4 * 2, anchor=tk.W)
-        self.mail.place(x=self.width / 3 + 10,
+        self.mail.place(x=self.width / 3 - 20,
                         y=self.height / 4 * 3, anchor=tk.W)
         avaBttn.place(x=self.width - 80, y=self.height - 20, anchor=tk.W)
         self.picPlace.place(x=self.width / 12, y=self.height / 2, anchor=tk.W)
 
 
 class InfoBox():
-    def __init__(self, parrent, width, x=0, y=0, lst: list = []) -> None:
-        self.width = width
-        self.lst = lst
+	def __init__(self, parrent, width, x=0, y=0, lst: list = []) -> None:
+		self.width = width
+		self.lst = lst
 
-        # Config
-        self.container = tk.Frame(parrent)
-        self.mainBox = tk.Canvas(
-            self.container, width=width, height=480, bg=LightGrey)
-        self.mainBox.pack_propagate(0)
+		# Config
+		self.container = tk.Frame(parrent)
+		self.mainBox = tk.Canvas(
+			self.container, width=width, height=480, bg=LightGrey)
+		self.mainBox.pack_propagate(0)
 
-        self.scrol = tk.Scrollbar(self.container, command=self.mainBox.yview)
-        self.scrollableFrame = tk.Frame(self.mainBox)
+		self.scrol = tk.Scrollbar(self.container, command=self.mainBox.yview)
+		self.scrollableFrame = tk.Frame(self.mainBox)
 
-        self.scrollableFrame.bind("<Configure>", lambda e: self.mainBox.configure(
-            scrollregion=self.mainBox.bbox("all")))
+		self.scrollableFrame.bind("<Configure>", lambda e: self.mainBox.configure(
+			scrollregion=self.mainBox.bbox("all")))
 
-        self.mainBox.create_window(
-            (0, 0), window=self.scrollableFrame, anchor=tk.NW)
-        self.mainBox.config(yscrollcommand=self.scrol.set)
+		self.mainBox.create_window(
+			(0, 0), window=self.scrollableFrame, anchor=tk.NW)
+		self.mainBox.config(yscrollcommand=self.scrol.set)
 
-        # FRAME  ->  CANVAS  ->  FRAME
-        #  ^          ^           ^
-        # Attach    Restrict     Scrollable
-        # Scrollbar  the field
+		# FRAME  ->  CANVAS  ->  FRAME
+		#  ^          ^           ^
+		# Attach    Restrict     Scrollable
+		# Scrollbar  the field
 
-        # Placement
-        self.container.place(x=x, y=y)
-        self.mainBox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.scrol.pack(side=tk.RIGHT, fill=tk.Y)
+		# Placement
+		self.container.place(x=x, y=y)
+		self.mainBox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+		self.scrol.pack(side=tk.RIGHT, fill=tk.Y)
 
-    def createBar(self, contact: Contact):
-        newBar = InfoBar(self.scrollableFrame, self.width - 10, contact.getName(),
-                         contact.getPhone(), contact.getEmail(), contact.getID())
+	def createBar(self, contact: Contact):
+		newBar = InfoBar(self.scrollableFrame, self.width - 10, contact.getName(),
+							contact.getPhone(), contact.getEmail(), contact.getID())
 
-    def newList(self, lst: list):
-        self.lst = lst
+	def newList(self, lst: list):
+		self.lst = lst
 
-    def refeshBox(self):
-        self.scrollableFrame.pack_forget()
-        for widget in self.scrollableFrame.winfo_children():
-            widget.destroy()
+	def refeshBox(self):
+		self.scrollableFrame.pack_forget()
+		for widget in self.scrollableFrame.winfo_children():
+			widget.destroy()
 
-        for contact in self.lst:
-            self.createBar(contact)
+		for contact in self.lst:
+			self.createBar(contact)
 
 
 class MainGUI():
     def __init__(self) -> None:
 
-        self.lst = testList1
+        self.lst = None
 
         # Phone book text
         phoneBookText = tk.Label(inputFrame, text='fonebook', font=(
@@ -189,93 +197,105 @@ class MainGUI():
         phoneBookText.place(x=30, y=5)
 
         credit = tk.Label(inputFrame, text="by Team 10, not Mark",
-                          font=("Comic Sans MS", 12), fg=Navy, bg=LightGrey)
-        credit.place(x=305, y=45)
+                            font=("Comic Sans MS", 12), fg=Navy, bg=LightGrey)
+        credit.place(x=300, y=45)
 
         # List Box
         self.listBox = InfoBox(inputFrame, 450, 470, 75)
-        self.listBox.newList(testList1)
-        self.listBox.refeshBox()
 
         # Refresh Button
-        refreshbttn = tk.Button(inputFrame, text="Refresh",
-                                command=self.listBox.refeshBox)
-        refreshbttn.place(x=900, y=10)
+        #refreshbttn = tk.Button(inputFrame, text="Refresh",
+        #                        command=self.listBox.refeshBox)
+        #refreshbttn.place(x=900, y=10)
 
         # Connect Button
         connectbttn = tk.Button(
-            inputFrame, text="Connect", bg=Bluely, fg="white", command= connectSocket)
-        connectbttn.place(x=840, y=10)
+            inputFrame, text="Connect", bg=Bluely, fg="white", command=connectSocket)
+        connectbttn.place(x=895, y=10)
+
+        # Display button
+        displaybttn = tk.Button(
+            inputFrame, text="Display All", bg=Bluely, fg="white", command= self.displayList)
+        displaybttn.place(x=680, y=45)
 
         # Search bar (Name)
         # Label
-        srcLabel = tk.Label(
+        srcNameLabel = tk.Label(
             inputFrame, text="Name", font=("Arial", 11), bg=LightGrey, fg="black"
         )
-        srcLabel.place(x=34, y=90)
+        srcNameLabel.place(x=34, y=90)
 
         # Bar
-        srcBox = tk.Entry(
+        srcNameBox = tk.Entry(
             inputFrame,  width=44
         )
-        #srcBox.place(x=85, y=106)
-        srcBox.place(x=36, y=116)
+        srcNameBox.place(x=36, y=116)
 
         # Button
-        srcBttn = tk.Button(
-            inputFrame, text="Search", bg=Bluely, fg="white", command=MainGUI.search
+        srcNameBttn = tk.Button(
+            inputFrame, text="Search", bg=Bluely, fg="white", command=lambda:MainGUI.searchName(self, srcNameBox.get())
         )
-        srcBttn.place(x=310, y=113)
+        srcNameBttn.place(x=310, y=113)
 
-        # Search bar (phonenumber)
+        # Search bar (phone number)
         # Label
-        srcLabel = tk.Label(
+        srcNumLabel = tk.Label(
             inputFrame, text="Phone number", font=("Arial", 11), bg=LightGrey, fg="black"
         )
-        srcLabel.place(x=34, y=140)
+        srcNumLabel.place(x=34, y=140)
 
         # Bar
-        srcBox = tk.Entry(
+        srcNumBox = tk.Entry(
             inputFrame,  width=44
         )
-        #srcBox.place(x=85, y=106)
-        srcBox.place(x=36, y=166)
+        srcNumBox.place(x=36, y=166)
 
         # Button
-        srcBttn = tk.Button(
-            inputFrame, text="Search", bg=Bluely, fg="white", command=MainGUI.search
+        srcNumBttn = tk.Button(
+            inputFrame, text="Search", bg=Bluely, fg="white", command=lambda:MainGUI.searchNum(self, srcNumBox.get())
         )
-        srcBttn.place(x=310, y=163)
+        srcNumBttn.place(x=310, y=163)
 
         # Search bar (email)
         # Label
-        srcLabel = tk.Label(
+        srcEmailLabel = tk.Label(
             inputFrame, text="Email", font=("Arial", 11), bg=LightGrey, fg="black"
         )
-        srcLabel.place(x=34, y=190)
+        srcEmailLabel.place(x=34, y=190)
 
         # Bar
-        srcBox = tk.Entry(
+        srcEmailBox = tk.Entry(
             inputFrame,  width=44
         )
-        #srcBox.place(x=85, y=106)
-        srcBox.place(x=36, y=216)
+        srcEmailBox.place(x=36, y=216)
 
         # Button
-        srcBttn = tk.Button(
-            inputFrame, text="Search", bg=Bluely, fg="white", command=MainGUI.search
+        srcEmailBttn = tk.Button(
+            inputFrame, text="Search", bg=Bluely, fg="white", command=lambda:MainGUI.searchEmail(self, srcEmailBox.get())
         )
-        srcBttn.place(x=310, y=213)
+        srcEmailBttn.place(x=310, y=213)
 
         mainWindow.mainloop()
 
-    def switchList(self):
-        self.lst = testList2
-        self.listBox.newList(testList2)
+    def displayList(self):
+        self.lst = sendAndRecv(COMMAND[1])
+        self.listBox.newList(self.lst)
         self.listBox.refeshBox()
 
-    def search():
-        print("hello")
+    def searchName(self, name: str):
+        self.lst = sendAndRecv(COMMAND[2]+name)
+        self.listBox.newList(self.lst)
+        self.listBox.refeshBox()
+
+    def searchNum(self, num: str):
+        self.lst = sendAndRecv(COMMAND[3]+num)
+        self.listBox.newList(self.lst)
+        self.listBox.refeshBox()
+
+    def searchEmail(self, email: str):
+        self.lst = sendAndRecv(COMMAND[4]+email)
+        self.listBox.newList(self.lst)
+        self.listBox.refeshBox()
 
     def ViewAva(id: str):
         frame = tk.Frame(inputFrame, width=267, height=267, bg="#000000")
@@ -284,7 +304,7 @@ class MainGUI():
             (267, 267)))  # Temporally using 00001.jpg for testing
         photo = tk.Label(frame, image=img)
         photo.image = img
-        photo.pack()
+        photo.pack()							
 
 if __name__ == "__main__":
     start = MainGUI()
